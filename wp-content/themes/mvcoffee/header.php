f18b650c5b80b81a3805c050beb2fba75dd3b45d
echizenyayota <virtualmisso30@gmail.com>
2017-01-04 10:01:30 +0900

    P106 テンプレートをパーツに分ける 4-1

--- wp-content/themes/mvcoffee/header.php
+++ wp-content/themes/mvcoffee/header.php
@@ -3,10 +3,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>Maverick Coffee</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="apple-touch-icon" href="./img/touch-icon.png" />
-  <link rel="shortcut icon" href="./favicon.ico" />
 
   <?php wp_head(); ?>
 
