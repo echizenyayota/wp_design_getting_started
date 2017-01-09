9d86e53b43c629d211835d8e49ac2d14221e2d70
echizenyayota <virtualmisso30@gmail.com>
2017-01-09 13:46:26 +0900

    P118 5-4-1 個別投稿用のテンプレートの準備と確認(single.phpの追加)

---
+++ wp-content/themes/mvcoffee/single.php
@@ -0,0 +1,122 @@
+<?php get_header(); ?>
+  <div id="wrap-container">
+
+    <div class="container">
+      <div class="row">
+
+        <main id="main-col" class="col-xs-12 col-md-9" role="main">
+          <div class="main-col__inner">
+
+
+            <article class="entry hentry">
+              <header class="entry-header">
+                <h1 class="entry-title">８月 新メニューのお知らせ</h1>
+                <div class="entry-thumbnail">
+                  <img src="./img/demo/img-postlist-000.jpg" alt="" class="img-thumbnail img-responsive"/>
+                </div>
+              </header>
+              <div class="entry-content clearfix">
+                <p>８月の新メニューが登場しました！</p>
+                <p>ランチタイムには季節の野菜をふんだんに使ったパニーニのＡセットが ￥８００円！ぜひこの機会にお召し上がりください～！</p>
+                <p>パニーニはフレッシュレタスと大人気のイタリアンハムを使用しました。使用しているパンの塩加減と甘さがとても相性良く、まさにランチタイムにぴったりなものとなっております。</p>
+                <p>お次は大人気のムーをご紹介します！</p>
+                <h2>ムーを使用したフレンチトースト</h2>
+                <p><img class="alignleft" src="./img/demo/img-post-001.jpg" alt=""/></p>
+                <p>大人気のフレンチトーストですが、こちらの人気の理由をご紹介します♪</p>
+                <p>外側はこんがりデニッシュのような食感で、中はタマゴが染みこんでとろとろ！大きさはそんなにありませんが、とても食べ応えのある一品です！</p>
+                <p>しっかりとフレンチトーストの味がついているのでそのままでも美味しく召し上がれますが、はちみつをつけて食べてもまた美味しいです！</p>
+                <p>一度食べたら忘れられないフレンチトースト！<br/>
+                  作りたてのバターの風味と香りを是非お試し下さい！</p>
+              </div>
+              <footer class="entry-footer">
+                <div class="entry-meta text-right">
+                  <div class="entry-meta__time">
+                    <span class="glyphicon glyphicon-time"></span><span class="vcard author"><a href="#" class="fn">Maverick staff</a></span> at <time datetime="2000-01-01">2000/00/00</time>
+                  </div>
+                  <div class="entry-meta__tag">
+                    <span class="glyphicon glyphicon-tag"></span>
+                    <span><a href="#" rel="tag">menu</a></span><span><a href="#" rel="tag">おすすめ</a><a href="#" rel="tag">ランチ</a></span>
+                  </div>
+                </div>
+              </footer>
+            </article>
+
+            <aside class="recommend">
+              <h2 class="recommend-title">関連記事</h2>
+              <ul class="row">
+                <li class="entry entry--simple recommend__post col-xs-12 col-sm-6 col-md-6">
+                  <div class="entry-thumbnail">
+                    <a href="#"><img src="./img/demo/img-postlist-005.jpg" alt="" class="img-responsive"/></a>
+                  </div>
+                  <h3 class="entry-title"><a href="#">７月 新メニューのお知らせ</a></h3>
+                </li>
+                <li class="entry entry--simple recommend__post col-xs-12 col-sm-6 col-md-6">
+                  <div class="entry-thumbnail">
+                    <a href="#"><img src="./img/demo/img-postlist-006.jpg" alt="" class="img-responsive"/></a>
+                  </div>
+                  <h3 class="entry-title"><a href="#">大人気のムーを使用したフレンチトースト</a></h3>
+                </li>
+                <li class="entry entry--simple recommend__post col-xs-12 col-sm-6 col-md-6">
+                  <div class="entry-thumbnail">
+                    <a href="#"><img src="./img/demo/img-postlist-007.jpg" alt="" class="img-responsive"/></a>
+                  </div>
+                  <h3 class="entry-title"><a href="#">パニーニとエスプレッソ</a></h3>
+                </li>
+                <li class="entry entry--simple recommend__post col-xs-12 col-sm-6 col-md-6">
+                  <div class="entry-thumbnail">
+                    <a href="#"><img src="./img/demo/img-postlist-008.jpg" alt="" class="img-responsive"/></a>
+                  </div>
+                  <h3 class="entry-title"><a href="#">暑い夏の特製デザートゼリー</a></h3>
+                </li>
+              </ul>
+            </aside>
+
+
+            <nav class="prevnext-nav">
+              <ul class="list-inline clearfix">
+                <li class="prevnext-nav__left pull-left"><a class="text-left font-serif" href="#">PREV</a></li>
+                <li class="prevnext-nav__right pull-right"><a class="text-right font-serif" href="#">NEXT</a></li>
+              </ul>
+            </nav>
+
+          </div>
+        </main>
+
+        <div id="sub-col" class="col-xs-12 col-md-3" role="complementary">
+          <aside class="widget">
+            <h2 class="widget__title font-serif">New Posts</h2>
+            <ul>
+              <li><a href="#">パニーニとエスプレッソ</a></li>
+              <li><a href="#">暑い夏の特製デザートゼリー</a></li>
+              <li><a href="#">８月の新メニューが登場しました！ランチタイムに…</a></li>
+              <li><a href="#">７月の定休日のお知らせです。第２水曜日を定休日…</a></li>
+              <li><a href="#">７月の新メニューが登場しました！中でも３種トマ…</a></li>
+            </ul>
+          </aside>
+
+          <aside class="widget">
+            <div class="widget--nogutter">
+              <a href="./gallery.html"><img class="img-responsive" src="./img/img-side-gallery.jpg" alt="Gallery"/></a>
+            </div>
+          </aside>
+
+          <aside class="widget">
+            <div class="widget--nogutter">
+              <a href="./access.html"><img class="img-responsive" src="./img/img-side-access.jpg" alt="Access"/></a>
+            </div>
+          </aside>
+
+          <aside class="widget">
+            <div class="widget--nogutter">
+              <a href="./menu.html"><img class="img-responsive" src="./img/img-side-menu.jpg" alt="Menu"/></a>
+            </div>
+          </aside>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <?php get_footer(); ?>
