805d45731631dd54cb4e09cd2b66df6e48f1e265
echizenyayota <virtualmisso30@gmail.com>
2017-01-12 09:45:46 +0900

    P141 投稿を呼び出すための準備

--- wp-content/themes/mvcoffee/archive.php
+++ wp-content/themes/mvcoffee/archive.php
@@ -8,80 +8,36 @@
         <main id="main-col" class="col-xs-12 col-md-9" role="main">
           <div class="main-col__inner">
 
-
-            <article class="entry entry--excerpt entry--archive">
-              <header>
-                <h2 class="entry-title"><a href="./single.html" rel="bookmark">８月 新メニューのお知らせ</a></h2>
-              </header>
-              <div class="entry-thumbnail">
-                <a href="./single.html"><img src="./img/demo/img-postlist-000.jpg" alt="" class="img-thumbnail img-responsive"/></a>
-              </div>
-              <div class="entry-content">
-                <p>８月の新メニューが登場しました！ランチタイムには季節の野菜をふんだんに揃えたＡセットが…</p>
-              </div>
-              <footer class="entry-meta text-right">
-                <div class="entry-meta__time">
-                  <span class="glyphicon glyphicon-time"></span><span class="vcard author"><a href="#" class="fn">Maverick staff</a></span> at <time class="updated" datetime="2015-08-01">2015/08/01</time>
-                </div>
-                <div class="entry-meta__tag">
-                  <span class="glyphicon glyphicon-tag"></span>
-                  <span><a href="./archive.html" rel="tag">新商品</a></span><span><a href="./archive.html" rel="tag">メニュー</a></span>
-                </div>
-              </footer>
-            </article>
-
-            <article class="entry entry--excerpt entry--archive">
-              <header>
-                <h2 class="entry-title"><a href="#" rel="bookmark">７月 新メニューのお知らせ</a></h2>
-              </header>
-              <div class="entry-thumbnail">
-                <a href="./single.html"><img src="./img/demo/img-postlist-005.jpg" alt="" class="img-thumbnail img-responsive"/></a>
-              </div>
-              <div class="entry-content">
-                <p>７月の新メニューが登場しました！中でも当店オリジナルのキーマカレーがおすすめです！</p>
-              </div>
-              <footer class="entry-meta text-right">
-                <div class="entry-meta__time">
-                  <span class="glyphicon glyphicon-time"></span><span class="vcard author"><a href="#" class="fn">Maverick staff</a></span> at <time class="updated" datetime="2015-07-04">2015/07/04</time>
-                </div>
-                <div class="entry-meta__tag">
-                  <span class="glyphicon glyphicon-tag"></span>
-                  <span><a href="./archive.html" rel="tag">新商品</a></span><span><a href="./archive.html" rel="tag">メニュー</a></span>
-                </div>
-              </footer>
-            </article>
-
-            <article class="entry entry--excerpt entry--archive">
-              <header>
-                <h2 class="entry-title"><a href="#" rel="bookmark">大人気のムーを使用したフレンチトースト</a></h2>
-              </header>
-              <div class="entry-thumbnail">
-                <a href="./single.html"><img src="./img/demo/img-postlist-006.jpg" alt="" class="img-thumbnail img-responsive"/></a>
-              </div>
-              <div class="entry-content">
-                <p>ムーを使用したフレンチトーストの販売を開始しました！ぜひ出来たてをお召し上がりください。</p>
-              </div>
-              <footer class="entry-meta text-right">
-                <div class="entry-meta__time">
-                  <span class="glyphicon glyphicon-time"></span><span class="vcard author"><a href="#" class="fn">Maverick staff</a></span> at <time class="updated" datetime="2015-05-27">2015/05/27</time>
-                </div>
-
-                <div class="entry-meta__tag">
-                  <span class="glyphicon glyphicon-tag"></span>
-                  <span><a href="./archive.html" rel="tag">新商品</a></span><span><a href="./archive.html" rel="tag">メニュー</a></span>
-                </div>
-              </footer>
-            </article>
-
-
-
-            <nav class="prevnext-nav">
-              <ul class="list-inline clearfix">
-                <li class="prevnext-nav__left pull-left"><a class="text-left font-serif" href="#">PREV</a></li>
-                <li class="prevnext-nav__right pull-right"><a class="text-right font-serif" href="#">NEXT</a></li>
-              </ul>
-            </nav>
-
+            <?php if (have_posts()) : ?>
+              <?php while( have_posts()) : the_post(); ?>
+                <article class="entry entry--excerpt entry--archive">
+                  <header>
+                    <h2 class="entry-title"><a href="./single.html" rel="bookmark">８月 新メニューのお知らせ</a></h2>
+                  </header>
+                  <div class="entry-thumbnail">
+                    <a href="./single.html"><img src="./img/demo/img-postlist-000.jpg" alt="" class="img-thumbnail img-responsive"/></a>
+                  </div>
+                  <div class="entry-content">
+                    <p>８月の新メニューが登場しました！ランチタイムには季節の野菜をふんだんに揃えたＡセットが…</p>
+                  </div>
+                  <footer class="entry-meta text-right">
+                    <div class="entry-meta__time">
+                      <span class="glyphicon glyphicon-time"></span><span class="vcard author"><a href="#" class="fn">Maverick staff</a></span> at <time class="updated" datetime="2015-08-01">2015/08/01</time>
+                    </div>
+                    <div class="entry-meta__tag">
+                      <span class="glyphicon glyphicon-tag"></span>
+                      <span><a href="./archive.html" rel="tag">新商品</a></span><span><a href="./archive.html" rel="tag">メニュー</a></span>
+                    </div>
+                  </footer>
+                </article>
+              <?php while; ?>
+              <nav class="prevnext-nav">
+                <ul class="list-inline clearfix">
+                  <li class="prevnext-nav__left pull-left"><a class="text-left font-serif" href="#">PREV</a></li>
+                  <li class="prevnext-nav__right pull-right"><a class="text-right font-serif" href="#">NEXT</a></li>
+                </ul>
+              </nav>
+           <?php endif; ?>
           </div>
         </main>
 
