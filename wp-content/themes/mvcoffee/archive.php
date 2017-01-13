234b5ac1ecf58d8bff06da1335f41632e30b8ffe
echizenyayota <virtualmisso30@gmail.com>
2017-01-13 19:18:00 +0900

    P143 コンテンツを表示する部分をテンプレートタグに修正

--- wp-content/themes/mvcoffee/archive.php
+++ wp-content/themes/mvcoffee/archive.php
@@ -10,27 +10,42 @@
 
             <?php if (have_posts()) : ?>
               <?php while( have_posts()) : the_post(); ?>
-                <article class="entry entry--excerpt entry--archive">
+                <article id ="post-<?php the_ID(); ?>" <?php post_class( 'entry entry--excerpt entry--archive'); ?>">
                   <header>
-                    <h2 class="entry-title"><a href="./single.html" rel="bookmark">８月 新メニューのお知らせ</a></h2>
+                    <h2 class="<?php the_permalink(); ?>"><a href="./single.html" rel="bookmark"><?php the_title(); ?></a></h2>
                   </header>
-                  <div class="entry-thumbnail">
-                    <a href="./single.html"><img src="./img/demo/img-postlist-000.jpg" alt="" class="img-thumbnail img-responsive"/></a>
-                  </div>
+
+
+                  <?php if ( has_post_thumbnail() ): ?>
+                    <div class="entry-thumbnail">
+                      <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('post_thumbnail', array(
+                            'class' => 'img-thumbnail img-responsive',
+                            'alt' => the_title_attribute('echo=0'),
+                            'title' => the_title_attribute('echo=0')
+                          )); ?>
+                      </a>
+                    </div>
+                  <?php endif; ?>
+
+
                   <div class="entry-content">
-                    <p>８月の新メニューが登場しました！ランチタイムには季節の野菜をふんだんに揃えたＡセットが…</p>
+                    <?php the_excerpt(); ?>
                   </div>
+
                   <footer class="entry-meta text-right">
                     <div class="entry-meta__time">
-                      <span class="glyphicon glyphicon-time"></span><span class="vcard author"><a href="#" class="fn">Maverick staff</a></span> at <time class="updated" datetime="2015-08-01">2015/08/01</time>
+                      <span class="glyphicon glyphicon-time"></span><span class="vcard author"><?php the_author_posts_link(); ?></span> at <time class="updated" datetime="<?php the_time( 'Y-m-d'); ?>"><?php the_time( 'Y/m/d'); ?></time>
                     </div>
-                    <div class="entry-meta__tag">
-                      <span class="glyphicon glyphicon-tag"></span>
-                      <span><a href="./archive.html" rel="tag">新商品</a></span><span><a href="./archive.html" rel="tag">メニュー</a></span>
-                    </div>
+                    <?php if (has_tag()) : ?>
+                      <div class="entry-meta__tag">
+                        <span class="glyphicon glyphicon-tag"></span>
+                        <?php the_tags('<span>', '</span><span>', '<span>'); ?>
+                      </div>
+                    <?php endif; ?>
                   </footer>
                 </article>
-              <?php while; ?>
+              <?php endwhile; ?>
               <nav class="prevnext-nav">
                 <ul class="list-inline clearfix">
                   <li class="prevnext-nav__left pull-left"><a class="text-left font-serif" href="#">PREV</a></li>
